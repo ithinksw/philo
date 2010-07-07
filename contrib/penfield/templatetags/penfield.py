889f3b6a7bb05b60aba7059031b9f3238d0d2a60
Kriti Godey <kriti.godey@gmail.com>
2010-07-07 12:51:22 +0530

    Added templatetags "monthname" and "ap_monthname" to Penfield, which turn month numbers into month names.

---
+++ contrib/penfield/templatetags/penfield.py
@@ -0,0 +1,23 @@
+from django import template
+from django.utils.dates import MONTHS, MONTHS_AP
+
+register = template.Library()
+
+def monthname(value):
+	monthnum = int(value)
+	if 1 <= monthnum <= 12:
+		return MONTHS[monthnum]
+	else:
+		return value
+
+register.filter('monthname', monthname)
+
+def ap_monthname(value):
+	monthnum = int(value)
+	if 1 <= monthnum <= 12:
+		return MONTHS_AP[monthnum]
+	else:
+		return value
+
+register.filter('monthname', monthname)
+register.filter('ap_monthname', ap_monthname)

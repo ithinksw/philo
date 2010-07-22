e83005f011574cca97a348cbcdf2dc539c22b2ff
melinath <stephen.r.burrows@gmail.com>
2010-07-22 18:37:31 -0400

    Added a bit to newsletters to streamline interactions with them.

--- contrib/penfield/models.py
+++ contrib/penfield/models.py
@@ -214,6 +214,7 @@ class NewsletterArticle(Entity, Titled):
 	full_text = models.TextField()
 	
 	class Meta:
+		get_latest_by = 'date'
 		ordering = ['-date']
 		unique_together = (('newsletter', 'slug'),)
 
@@ -224,7 +225,7 @@ register_value_model(NewsletterArticle)
 class NewsletterIssue(Entity, Titled):
 	newsletter = models.ForeignKey(Newsletter, related_name='issues')
 	number = models.PositiveIntegerField()
-	articles = models.ManyToManyField(NewsletterArticle)
+	articles = models.ManyToManyField(NewsletterArticle, related_name='issues')
 	
 	class Meta:
 		ordering = ['-number']

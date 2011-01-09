184000ec978f6c0119bdb94a0cd24dbb84a09125
Kriti Godey <kriti.godey@gmail.com>
2011-01-09 17:09:12 -0500

    Initial model classes written. Still a few additions needed.

--- contrib/julian/models.py
+++ contrib/julian/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from philo.models.base import Tag, Entity, Titled
+import datetime
 
-# Create your models here.
+class Location(Entity, Titled):
+	slug = models.SlugField(max_length=255, unique=True)
+
+
+class Calendar(Entity, Titled):
+	slug = models.SlugField(max_length=255, unique=True)
+
+
+class Event(Entity, Titled):
+	description = models.TextField()
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
+	is_all_day_event = models.BooleanField(default=False)
+	time_created = models.DateTimeField(default=datetime.datetime.now)
+	location = models.ForeignKey(Location)
+	calendars = models.ManyToManyField(Calendar)
+	tags = models.ManyToManyField(Tag)
+

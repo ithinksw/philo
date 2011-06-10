3cbd4bd0a4f67874f7cfb157879f84b98425d8cb
Harris Lapiroff <hlapirof@oberlin.edu>
2011-06-10 09:57:00 -0400

    Moved the config.rb file to a place where it makes more sense. Added a readme.

--- philo/contrib/gilbert/static/gilbert/murano/config.rb
+++ philo/contrib/gilbert/static/gilbert/murano/config.rb
@@ -1,17 +1,19 @@
 # $ext_path: This should be the path of where the ExtJS SDK is installed
 # Generally this will be in a lib/extjs folder in your applications root
 # <root>/lib/extjs
-$ext_path = "../../extjs"
+$ext_path = "../extjs"
+
+project_path = File.dirname(__FILE__)
 
 # sass_path: the directory your Sass files are in. THIS file should also be in the Sass folder
 # Generally this will be in a resources/sass folder
 # <root>/resources/sass
-sass_path = File.dirname(__FILE__)
+sass_path = File.join(project_path, 'sass')
 
 # css_path: the directory you want your CSS files to be.
 # Generally this is a folder in the parent directory of your Sass files
 # <root>/resources/css
-css_path = File.join(sass_path, "..", "css")
+css_path = File.join(project_path, "css")
 
 # output_style: The output style for your compiled CSS
 # nested, expanded, compact, compressed
@@ -20,4 +22,4 @@ output_style = :nested
 environment = :development
 
 # We need to load in the Ext4 themes folder, which includes all it's default styling, images, variables and mixins
-load File.join(File.dirname(__FILE__), $ext_path, 'resources', 'themes')
+load File.join(project_path, $ext_path, 'resources', 'themes')

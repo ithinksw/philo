e8b51a396d27271aa8b8d06724a21dbe2f0ac895
Joseph Spiros <joseph.spiros@ithinksw.com>
2012-02-01 15:16:23 -0500

    Fixed a few bugs with the documentation, and links to the documentation from the README. Sphinx can now build the documentation reliably due to conf.py explicitly adding the docs directory to the path. Added a link to the Bitbucket mirror, extending the set currently consisting of GitHub and Gitorious.

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext")))
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
